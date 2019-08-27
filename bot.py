6dde735cac24544ba224fd9e7d8761b837d71134
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-27 18:29:08 -0400

    Filtering for attachments

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import re
 import discord
 import os
 from random import choice
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix='!')  # subclass, Client is superclass
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to discord')
+   print(f'{bot.user.name} has connected to discord')
 
 
 @bot.command(name="hello", help="gives a random quote")
@@ -53,24 +53,60 @@ async def create_channel(ctx, channel_name='real-bot-channel'):
 
 
 @bot.event
-@commands.has_role('admeeeen')
 async def on_message(message):
+    if message.author == bot.user:
+        return
     print(message)
+    print(f'Pin this {message.content}')
+    print(f' Message has attachment? {message.attachments}')
+    # elif message.content.startswith('durhurhur'):
+    print('-----------------------------')
 
-    if message.content.startswith('durhurhur'):
-        channel = message.channel
+    if message.content.startswith('history'):
+        msg = await message.channel.history(limit=50).flatten()
+        with_attachments = [];
+        print(f'After getting msgs {msg}')
+        for m in msg:
+            if m.attachments:
+                print(f'This m : {m.content}')
+                print(f'Found attachment {m.attachments}')
+                with_attachments.append(m)
+        print('--------------------------------------')
+        print(f'After filter, {with_attachments}')
+        await message.channel.send()
+    elif message.content.startswith('react'):
 
-        await channel.send('Thumbs up me pls :3 ')
+    #     channel = message.channel
+    #
+    #     await channel.send('Thumbs up me pls :3 ')
+    #
+    #     def check(reaction, user):
+    #         return user == message.author and str(reaction.emoji) == '👍'
+    #
+    #     try:
+    #         reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+    #     except asyncio.TimeoutError:
+    #         await channel.send(':(')
+    #     else:
+    #         await channel.send(f'I <3 you too, <@{message.author.id}>')
+    # elif message.content.startswith('pin this'):
+    #     print(message)
+    #     print(f'Pin this {message.content}')
+    #     print(f' Message has attachment? {message.attachments}')
+    # elif '2dlive' in message.content.lower() or re.search(r'\b2d live\b', message.content):
+    #     #     print(f'found 2d live ', message.content)
+    #     #     channel = message.channel
+    #     #     random_filler = [
+    #     #         f'It is a wonderful day, dear <@{message.author.id}>, but perhaps you meant ***Live2D***.',
+    #     #         f'Welcome to the Live2D hell,  <@{message.author.id}>, ***Live2D***',
+    #     #         f'A puppy gets sad every time someone says 2dlive. A puppy gets an extra treat every time someone says ***Live2D***',
+    #     #         f'Beep-bop-beep-bop, robot detected 2dLive, wants to tell <@{message.author.id}> that it is ***Live2D***.s'
+    #     #     ]
+    #     #
+    #     #     choice_of_tease = choice(random_filler)
+    #     #     await channel.send(choice_of_tease)
 
-        def check(reaction, user):
-            return user == message.author and str(reaction.emoji) == '👍'
 
-        try:
-            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
-        except asyncio.TimeoutError:
-            await channel.send(':(')
-        else:
-            await channel.send(f'I <3 you too, <@{message.author.id}>')
 
 
 @bot.event
@@ -79,6 +115,7 @@ async def on_command_error(ctx, error):
     print(error)
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the permissions to do this.')
+
 
 
 
