fcb2c1a99679bba252fc66293fa3408e0f373681
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-27 23:42:19 -0400

    Refactor: moving some on_message events to commands.

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-import asyncio
-import datetime
+
 import re
 import discord
 import os
@@ -12,6 +11,27 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 bot = commands.Bot(command_prefix='!')  # subclass, Client is superclass
+admin = 'admeeeen'
+url = r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](' \
+      r'?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad' \
+      r'|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca' \
+      r'|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk' \
+      r'|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir' \
+      r'|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml' \
+      r'|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn' \
+      r'|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td' \
+      r'|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw' \
+      r')/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s(' \
+      r')]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](' \
+      r'?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad' \
+      r'|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca' \
+      r'|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk' \
+      r'|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir' \
+      r'|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml' \
+      r'|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn' \
+      r'|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td' \
+      r'|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw' \
+      r')\b/?(?!@))) '
 
 
 @bot.event
@@ -19,48 +39,53 @@ async def on_ready():
     print(f'{bot.user.name} has connected to discord')
 
 
-@bot.command(name='create-channel', help='Makes a new channel. Admin only.')
-@commands.has_role('admeeeen')
+@bot.command(name='create-channel', help='[Admin only] Makes a new channel.')
+@commands.has_role(admin)
 async def create_channel(ctx, channel_name='unnamed-channel'):
     guild = ctx.guild
-    print(guild.channels)
     existing_channel = discord.utils.get(guild.channels, name=channel_name)  # if true, a same name channel exists
-    print(f'Utils.get : {existing_channel}')
     if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
+        await ctx.message.channel.send(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
+
+
+@bot.command(name='tag-posts', help='[Admin only] Tags all posts with attachment. ')
+@commands.has_role(admin)
+async def tag_posts(ctx):
+    channel = ctx.message.channel
+    author = ctx.message.author
+    count = 0
+    if channel:
+        msg = await channel.history(limit=50).flatten()
+        await channel.send(f'<@{author.id}> commenced post tagging.')
+        for m in msg:
+            if m.attachments or re.search(url, m.content):
+                await m.add_reaction('📌')
+                count += 1
+        await channel.send(f'Tagged {count} post(s), <@{author.id}>')
+
+
+@bot.command(name='clean-up', help='[Admin only] Remove all posts without attachments. ')
+@commands.has_role(admin)
+async def clean_up(ctx):
+    channel = ctx.message.channel
+    count = 0
+    if channel:
+        await channel.send(f'📢 Beep-beep. Clean up time. All posts without attachments will be removed.')
+        msg = await channel.history(limit=50).flatten()
+        for m in msg:
+            if not m.reactions:
+                count += 1
+                await m.delete()
+        await channel.send(f'📢 Helpful robot removed {count} post(s). I am so helpful')
 
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.content.startswith('tag_posts'):
-        msg = await message.channel.history(limit=50).flatten()
-        await message.channel.send(f'<@{message.author.id}> commenced post tagging.')
-        for m in msg:
-            if m.attachments:
-                await m.add_reaction('📌')
-        await message.channel.send(f'Tagging completed, <@{message.author.id}>')
 
-    elif message.content.startswith('clean_up_posts'):
-        msg = await message.channel.history(limit=50).flatten()
-        await message.channel.send(f'📢 Beep-beep. Clean up time. All posts without attachments will be removed.')
-        for m in msg:
-            if not m.reactions:
-                await m.delete()
-        await message.channel.send('📢 All posts without attachments have now been removed. I am so helpful.')
-
-    elif message.content.startswith('remove react'):
-        msg = await message.channel.history(limit=50).flatten()
-        print(f'After getting msgs {msg}')
-        for m in msg:
-            if m.reactions:
-                print(f'This m : {m.content}')
-                print(f'Found reactions {m.reactions}')
-                await m.remove_reaction('📌', bot.user)
-
-    elif '2dlive' in message.content.lower() or re.search(r'\b2d live\b', message.content):
+    if '2dlive' in message.content.lower() or re.search(r'\b2d live\b', message.content.lower()):
         channel = message.channel
         random_filler = [
             f'It is a wonderful day, dear <@{message.author.id}>, but perhaps you meant ***Live2D***.',
@@ -72,6 +97,14 @@ async def on_message(message):
 
         choice_of_tease = choice(random_filler)
         await channel.send(choice_of_tease)
+
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send(f'<@{ctx.message.author.id}>, I love you, but you do not have the permissions to do that.')
 
 
 bot.run(TOKEN)
